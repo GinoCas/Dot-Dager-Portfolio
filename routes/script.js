4295ed3419fbbfef33f6cbfcfcb0e090d46503db
EquipoTC <gino.casentini.5@gmail.com>
2024-11-23 05:31:33 -0300

    feat(frontend): add lumberjack

--- routes/script.js
+++ routes/script.js
@@ -3,3 +3,16 @@ window.onload = function () {
     window.scrollTo(0, 0);
   }, 15);
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lumberjackContainer = document.getElementById("lumberjack-container");
+  const openButton = document.getElementById("show-game");
+  const closeButton = document.getElementById("close-game");
+
+  openButton.addEventListener("click", () => {
+    lumberjackContainer.classList.toggle("show");
+  });
+  closeButton.addEventListener("click", () => {
+    lumberjackContainer.classList.toggle("show");
+  });
+});
